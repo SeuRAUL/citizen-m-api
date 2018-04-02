75ed07abc4c2ef86bc592c2804b12bd2b7b19e90
SeuRaul <seuraul.0@gmail.com>
2018-04-02 12:36:49 +0000

    Citizen edit

--- app/controllers/api/v1/citizens_controller.rb
+++ app/controllers/api/v1/citizens_controller.rb
@@ -13,6 +13,36 @@ module Api
 				citizen = Citizen.find(params[:id])
 				render json: {status: 'SUCCESS', message:'Loaded citizen', data:citizen}, status: :ok
 			end
+			
+			# Create new citizen
+			def create
+				citizen = Citizen.new(citizen_params)
+				if citizen.save
+					render json: {status: 'SUCCESS', message:'Saved citizen', data:citizen}, status: :ok
+				else
+					render json: {status: 'ERROR', message:'Citizens not saved', data:citizen.erros}, status: :unprocessable_entity
+				end
+			end
+			
+			# Update last location
+			def update
+				citizen = Citizen.find(params[:id])
+				if citizen.update_attributes(citizen_update_params)
+					render json: {status: 'SUCCESS', message:'Updated citizen', data:citizen}, status: :ok
+				else
+					render json: {status: 'ERROR', message:'Citizen not updated', data:citizen.erros}, status: :unprocessable_entity
+				end
+			end
+			
+			# Permited Params
+			private
+			def citizen_params
+				params.permit(:name, :age, :gender, :lastLocation)
+			end
+			
+			def citizen_update_params
+				params.permit(:lastLocation)
+			end
             
         end
     end
