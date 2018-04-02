6756714e0b8a065fd0faad297e268ee8f21919d1
SeuRaul <seuraul.0@gmail.com>
2018-04-02 12:14:38 +0000

    Citizen by ID

--- app/controllers/api/v1/citizens_controller.rb
+++ app/controllers/api/v1/citizens_controller.rb
@@ -4,9 +4,15 @@ module Api
             
             # List all citizens
             def index
-               citizens = Citizens.order('created_at DESC');
+               citizens = Citizen.order('created_at DESC');
                render json: {status: 'SUCCESS', message:'Citizens loaded', data:citizens}, status: :ok
             end
+            
+            # List citizen by ID
+			def show
+				citizen = Citizen.find(params[:id])
+				render json: {status: 'SUCCESS', message:'Loaded citizen', data:citizen}, status: :ok
+			end
             
         end
     end
